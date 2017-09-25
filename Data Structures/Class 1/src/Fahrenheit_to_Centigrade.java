310e6237c506dd754cd000611231bd06951007ad
Ben Avery <strider8746@gmail.com>
2017-09-24 23:13:53 -0400

    TRYING TO UPDATE THE REMOTE.

--- Data Structures/Class 1/src/Fahrenheit_to_Centigrade.java
+++ Data Structures/Class 1/src/Fahrenheit_to_Centigrade.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class Fahrenheit_to_Centigrade {
+    public static void FtoC() {
+        Scanner scr = new Scanner(System.in);
+        double f;
+        System.out.println("Please enter degrees Fahrenheit to convert:");
+        f = scr.nextDouble();
+        double c = (f - 32.0)*(5.0/9.0);
+        System.out.println(f + " degrees Fahrenheit equals " + c + " degrees Centigrade.");
+        return;
+    }
 }
